4fc0304c758fae79816bc5393f134d905094d5b8
Michel de Bree <michel@micheldebree.nl>
2016-06-10 22:00:03 +0200

    Moved image data remapping from vicar to Retropixels.

---
+++ Remapper.js
@@ -0,0 +1,33 @@
+ /*global PixelImage, ColorMap */
+function Remapper(targetPixelImage) {
+    'use strict';
+    this.targetPixelImage = targetPixelImage;
+}
+ 
+Remapper.prototype.getColorMap = function (imageData, image) {
+    'use strict';
+    var w = imageData.width,
+     h = imageData.height,
+     unrestrictedImage = PixelImage.create(w, h, new ColorMap(w, h, 1, 1), image.pWidth, image.pHeight);
+
+    unrestrictedImage.palette = image.palette;
+    unrestrictedImage.dither = image.dither;
+    unrestrictedImage.errorDiffusionDither = image.errorDiffusionDither;
+    unrestrictedImage.drawImageData(imageData);
+    return unrestrictedImage.colorMaps[0];
+};
+ 
+Remapper.prototype.mapImageData = function (imageData) {
+    'use strict';
+
+    var ci, 
+     colorMap = this.getColorMap(imageData, this.targetPixelImage);
+
+    // fill up the colormaps in the restricted image based on the colors in the unrestricted image
+    for (ci = 0; ci < this.targetPixelImage.colorMaps.length; ci += 1) {
+     colorMap.extractColorMap(this.targetPixelImage.colorMaps[ci]);
+    }
+
+    // draw the image again in the restricted image
+    this.targetPixelImage.drawImageData(imageData);
+ };
