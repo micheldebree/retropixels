921b3cd72374c49a54399512c2ac7de20788712d
Michel de Bree <michel@micheldebree.nl>
2016-10-11 20:32:01 +0200

    Refactoring

--- GraphicModes.js
+++ GraphicModes.js
@@ -1,45 +1,68 @@
 var PixelImage = require('./PixelImage.js'),
-    ColorMap = require('./ColorMap.js');
+    ColorMap = require('./ColorMap.js'),
     Palette = require('./Palette.js');
 
-c64Multicolor = function() {
-    'use strict';
-    var pixelImage = PixelImage.create(160, 200, undefined, 2, 1);
-    pixelImage.palette = Palette.peptoPalette;
-    pixelImage.colorMaps.push(new ColorMap(160, 200));
-    pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 8));
-    pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 8));
-    pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 8));
-    return pixelImage;
+peptoPalette = new Palette([
+    [0, 0, 0, 0xff], // black
+    [0xff, 0xff, 0xff, 0xff], // white
+    [0x68, 0x37, 0x2b, 0xff], //red
+    [0x70, 0xa4, 0xb2, 0xff], //cyan
+    [0x6f, 0x3d, 0x86, 0xff], //purple
+    [0x58, 0x8d, 0x43, 0xff], //green
+    [0x35, 0x28, 0x79, 0xff], //blue
+    [0xb8, 0xc7, 0x6f, 0xff], //yellow
+    [0x6f, 0x4f, 0x25, 0xff], //orange
+    [0x43, 0x39, 0x00, 0xff], //brown
+    [0x9a, 0x67, 0x59, 0xff], //light red
+    [0x44, 0x44, 0x44, 0xff], //dark gray
+    [0x6c, 0x6c, 0x6c, 0xff], //medium gray
+    [0x9a, 0xd2, 0x84, 0xff], //light green
+    [0x6c, 0x5e, 0xb5, 0xff], //light blue
+    [0x95, 0x95, 0x95, 0xff]  //green
+]);
+
+c64Multicolor = {
+    create: function() {
+        var pixelImage = PixelImage.create(160, 200, undefined, 2, 1);
+        pixelImage.palette = peptoPalette;
+        pixelImage.colorMaps.push(new ColorMap(160, 200));
+        pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 8));
+        pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 8));
+        pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 8));
+        return pixelImage;
+    }
 };
 
-c64FLI = function() {
-    'use strict';
-    var pixelImage = PixelImage.create(160, 200, undefined, 2, 1);
-    pixelImage.palette = Palette.peptoPalette;
-    pixelImage.colorMaps.push(new ColorMap(160, 200));
-    pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 8));
-    pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 1));
-    pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 1));
-    return pixelImage;
+c64FLI = {
+    create: function() {
+        var pixelImage = PixelImage.create(160, 200, undefined, 2, 1);
+        pixelImage.palette = peptoPalette;
+        pixelImage.colorMaps.push(new ColorMap(160, 200));
+        pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 8));
+        pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 1));
+        pixelImage.colorMaps.push(new ColorMap(160, 200, 4, 1));
+        return pixelImage;
+    }
 };
 
-c64AFLI = function() {
-    'use strict';
-    var pixelImage = PixelImage.create(320, 200, undefined, 1, 1);
-    pixelImage.palette = Palette.peptoPalette;
-    pixelImage.colorMaps.push(new ColorMap(320, 200, 8, 8));
-    pixelImage.colorMaps.push(new ColorMap(320, 200, 8, 1));
-    return pixelImage;
+c64AFLI = {
+    create: function() {
+        var pixelImage = PixelImage.create(320, 200, undefined, 1, 1);
+        pixelImage.palette = peptoPalette;
+        pixelImage.colorMaps.push(new ColorMap(320, 200, 8, 8));
+        pixelImage.colorMaps.push(new ColorMap(320, 200, 8, 1));
+        return pixelImage;
+    }
 };
 
-c64Hires = function() {
-    'use strict';
-    var pixelImage = PixelImage.create(320, 200, undefined, 1, 1);
-    pixelImage.palette = Palette.peptoPalette;
-    pixelImage.colorMaps.push(new ColorMap(320, 200, 8, 8));
-    pixelImage.colorMaps.push(new ColorMap(320, 200, 8, 8));
-    return pixelImage;
+c64Hires = {
+    create: function() {
+        var pixelImage = PixelImage.create(320, 200, undefined, 1, 1);
+        pixelImage.palette = peptoPalette;
+        pixelImage.colorMaps.push(new ColorMap(320, 200, 8, 8));
+        pixelImage.colorMaps.push(new ColorMap(320, 200, 8, 8));
+        return pixelImage;
+    }
 };
 
 module.exports = {
--- Palette.js
+++ Palette.js
@@ -5,16 +5,15 @@ function Palette(pixels) {
     this.pixels = pixels === undefined ? [] : pixels;
 }
 
-Palette.prototype.get = function (index) {
+Palette.prototype.get = function(index) {
     'use strict';
     return this.pixels[index];
 };
 
-
-Palette.prototype.getDistance = function (onePixel, index, offsetPixel, weight) {
+Palette.prototype.getDistance = function(onePixel, index, offsetPixel, weight) {
     'use strict';
     var otherPixel = this.pixels[index];
-    
+
     offsetPixel = offsetPixel !== undefined ? offsetPixel : PixelCalculator.emptyPixel;
     weight = weight !== undefined ? weight : [1, 1, 1];
 
@@ -23,20 +22,18 @@ Palette.prototype.getDistance = function (onePixel, index, offsetPixel, weight) 
     offsetPixel = PixelCalculator.toYUV(offsetPixel);
 
     return Math.sqrt(
-       weight[0] * Math.pow(onePixel[0] - otherPixel[0] - offsetPixel[0], 2) +
-       weight[1] * Math.pow(onePixel[1] - otherPixel[1] - offsetPixel[1], 2) +
-       weight[2] * Math.pow(onePixel[2] - otherPixel[2] - offsetPixel[2], 2)
-   );
-
+        weight[0] * Math.pow(onePixel[0] - otherPixel[0] - offsetPixel[0], 2) +
+        weight[1] * Math.pow(onePixel[1] - otherPixel[1] - offsetPixel[1], 2) +
+        weight[2] * Math.pow(onePixel[2] - otherPixel[2] - offsetPixel[2], 2)
+    );
 };
 
 /**
  * Map a pixel to the closest available color in the palette.
  * @returns the index into the palette
  */
-Palette.prototype.mapPixel = function (pixel, offset, weight) {
+Palette.prototype.mapPixel = function(pixel, offset, weight) {
     'use strict';
-
     var i,
         d,
         minVal,
@@ -52,29 +49,7 @@ Palette.prototype.mapPixel = function (pixel, offset, weight) {
             minI = i;
         }
     }
-
     return minI;
-
 };
-
-// TODO: put this somewhere but not in a global variable
-Palette.peptoPalette = new Palette([
-    [0, 0, 0, 0xff], // black
-    [0xff, 0xff, 0xff, 0xff], // white
-    [0x68, 0x37, 0x2b, 0xff], //red
-    [0x70, 0xa4, 0xb2, 0xff], //cyan
-    [0x6f, 0x3d, 0x86, 0xff], //purple
-    [0x58, 0x8d, 0x43, 0xff], //green
-    [0x35, 0x28, 0x79, 0xff], //blue
-    [0xb8, 0xc7, 0x6f, 0xff], //yellow
-    [0x6f, 0x4f, 0x25, 0xff], //orange
-    [0x43, 0x39, 0x00, 0xff], //brown
-    [0x9a, 0x67, 0x59, 0xff], //light red
-    [0x44, 0x44, 0x44, 0xff], //dark gray
-    [0x6c, 0x6c, 0x6c, 0xff], //medium gray
-    [0x9a, 0xd2, 0x84, 0xff], //light green
-    [0x6c, 0x5e, 0xb5, 0xff], //light blue
-    [0x95, 0x95, 0x95, 0xff]  //green
-]);
 
 module.exports = Palette;
