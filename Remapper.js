078bdade6eea65fed36075a1fcda5d06dc3e00ca
Michel de Bree <michel@micheldebree.nl>
2016-10-12 13:16:21 +0200

    Remapper no longer draws the image, just optimizes ColorMaps

--- Remapper.js
+++ Remapper.js
@@ -1,15 +1,11 @@
   var PixelImage = require('./PixelImage');
   var ColorMap = require('./ColorMap');
 
-  /**
-  Convert ImageData to a ColorMap, using the same properties as the target image.
-  */
   function getColorMap(imageData, targetPixelImage) {
       'use strict';
       var w = imageData.width,
           h = imageData.height,
           unrestrictedImage = PixelImage.create(w, h, new ColorMap(w, h, 1, 1), targetPixelImage.pWidth, targetPixelImage.pHeight);
-
       unrestrictedImage.palette = targetPixelImage.palette;
       unrestrictedImage.dither = targetPixelImage.dither;
       unrestrictedImage.mappingWeight = targetPixelImage.mappingWeight;
@@ -18,12 +14,8 @@
       return unrestrictedImage.colorMaps[0];
   }
 
-  /**
-  Map ImageData onto the target image.
-  */
-  function mapImageData(imageData, targetPixelImage) {
+  function optimizeColorMaps(imageData, targetPixelImage) {
       'use strict';
-
       var ci,
           colorMap = getColorMap(imageData, targetPixelImage);
 
@@ -31,11 +23,8 @@
       for (ci = 0; ci < targetPixelImage.colorMaps.length; ci += 1) {
           colorMap.extractColorMap(targetPixelImage.colorMaps[ci]);
       }
-
-      // draw the image again in the restricted image
-      targetPixelImage.drawImageData(imageData);
   }
 
 module.exports = {
-  mapImageData : mapImageData
+  optimizeColorMaps : optimizeColorMaps
 };
