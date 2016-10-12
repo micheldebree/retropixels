cc52d4a9e3dda767a9b33a56afe541c9369e074c
Michel de Bree <michel@micheldebree.nl>
2016-10-12 12:56:23 +0200

    Refactored Remapper

--- Remapper.js
+++ Remapper.js
@@ -1,43 +1,41 @@
   var PixelImage = require('./PixelImage');
   var ColorMap = require('./ColorMap');
 
-  function Remapper(targetPixelImage) {
-      this.targetPixelImage = targetPixelImage;
-  }
-
   /**
   Convert ImageData to a ColorMap, using the same properties as the target image.
   */
-  Remapper.prototype.getColorMap = function(imageData) {
+  function getColorMap(imageData, targetPixelImage) {
       'use strict';
       var w = imageData.width,
           h = imageData.height,
-          unrestrictedImage = PixelImage.create(w, h, new ColorMap(w, h, 1, 1), this.targetPixelImage.pWidth, this.targetPixelImage.pHeight);
+          unrestrictedImage = PixelImage.create(w, h, new ColorMap(w, h, 1, 1), targetPixelImage.pWidth, targetPixelImage.pHeight);
 
-      unrestrictedImage.palette = this.targetPixelImage.palette;
-      unrestrictedImage.dither = this.targetPixelImage.dither;
-      unrestrictedImage.mappingWeight = this.targetPixelImage.mappingWeight;
-      unrestrictedImage.errorDiffusionDither = this.targetPixelImage.errorDiffusionDither;
+      unrestrictedImage.palette = targetPixelImage.palette;
+      unrestrictedImage.dither = targetPixelImage.dither;
+      unrestrictedImage.mappingWeight = targetPixelImage.mappingWeight;
+      unrestrictedImage.errorDiffusionDither = targetPixelImage.errorDiffusionDither;
       unrestrictedImage.drawImageData(imageData);
       return unrestrictedImage.colorMaps[0];
-  };
+  }
 
   /**
   Map ImageData onto the target image.
   */
-  Remapper.prototype.mapImageData = function(imageData) {
+  function mapImageData(imageData, targetPixelImage) {
       'use strict';
 
       var ci,
-          colorMap = this.getColorMap(imageData);
+          colorMap = getColorMap(imageData, targetPixelImage);
 
       // fill up the colormaps in the restricted image based on the colors in the unrestricted image
-      for (ci = 0; ci < this.targetPixelImage.colorMaps.length; ci += 1) {
-          colorMap.extractColorMap(this.targetPixelImage.colorMaps[ci]);
+      for (ci = 0; ci < targetPixelImage.colorMaps.length; ci += 1) {
+          colorMap.extractColorMap(targetPixelImage.colorMaps[ci]);
       }
 
       // draw the image again in the restricted image
-      this.targetPixelImage.drawImageData(imageData);
-  };
+      targetPixelImage.drawImageData(imageData);
+  }
 
-module.exports = Remapper;
+module.exports = {
+  mapImageData : mapImageData
+};
