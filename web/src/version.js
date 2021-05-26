d22143fe4299a910e3b1e64907d9cd26474c3513
Michel de Bree <michel@micheldebree.nl>
2021-05-26 08:14:27 +0200

    Fix for image in twitter card and opengraph data.

--- web/src/version.js
+++ web/src/version.js
@@ -1,1 +1,1 @@
-export default {buildnr: '2021-05-25.cfdfa1d', url: 'https://github.com/micheldebree/retropixels/commit/cfdfa1d4a0213e38daf1616bce7b91ab8a41d658'}
+export default {buildnr: '2021-05-26.14602fd', url: 'https://github.com/micheldebree/retropixels/commit/14602fd13eec0f67ea478c1d811c99edeb8a73bc'}
