aaa8c9812bb44c789db5d17f5c1486d73c8e9814
Michel de Bree <michel@micheldebree.nl>
2021-05-27 07:20:00 +0200

    New retropixels release

---
+++ web/release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# make build
+# SITE="../../website/public"
+# APP="${SITE}/retropixels"
+# rm -rf ${APP}
+# mv ./build ${APP}
+
+cd "${SITE}" && git add . && git commit -m "New retropixels release" && git push
+
+
--- web/src/version.js
+++ web/src/version.js
@@ -1,1 +1,1 @@
-export default {buildnr: '2021-05-26.d22143f', url: 'https://github.com/micheldebree/retropixels/commit/d22143fe4299a910e3b1e64907d9cd26474c3513'}
+export default {buildnr: '2021-05-27.38520da', url: 'https://github.com/micheldebree/retropixels/commit/38520da30997ae7abffbf782fd0758a6ca9d13c7'}
