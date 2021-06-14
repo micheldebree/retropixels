c1fe25421072a32d9da9608a891dfa9d0990003f
Michel de Bree <michel@micheldebree.nl>
2021-06-14 08:23:19 +0200

    Adds a button to download the output image as PNG file (#67)

--- web/src/version.js
+++ web/src/version.js
@@ -1,4 +1,4 @@
 export default {
-  buildnr: '2021-06-05.2e86f11',
-  url: 'https://github.com/micheldebree/retropixels/commit/2e86f11f2ed45ba9e1109f7a0fdd1c72c83ee83e'
+  buildnr: '2021-06-12.860cf59',
+  url: 'https://github.com/micheldebree/retropixels/commit/860cf59dc205b7a598dfc710c1b60809885bdc21'
 };
