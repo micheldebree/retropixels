7b1417007c787b2db40732eae24c0097dc83f1bc
Michel de Bree <michel@micheldebree.nl>
2022-04-08 11:31:20 +0200

    Upgraded web to React 18

--- web/src/version.js
+++ web/src/version.js
@@ -1,4 +1,4 @@
 export default {
-  buildnr: '2021-06-12.860cf59',
-  url: 'https://github.com/micheldebree/retropixels/commit/860cf59dc205b7a598dfc710c1b60809885bdc21'
+  buildnr: '2022-04-07.334531d',
+  url: 'https://github.com/micheldebree/retropixels/commit/334531d7542c3406593a05008c8833f0b991416e'
 };
